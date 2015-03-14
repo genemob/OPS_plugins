3f87add3f630d88c11499c52b4d088aad3fea93a
genemob <piotr@genemob.com>
2015-03-14 13:06:07 -0400

    Started working on EPRegisterPlugin in dev branch.

--- patmob/plugin/ops/register/EPRegisterPlugin.java
+++ patmob/plugin/ops/register/EPRegisterPlugin.java
@@ -1,9 +1,21 @@
 package patmob.plugin.ops.register;
 
+import patmob.core.PatmobPlugin;
+
 /**
  *
  * @author Piotr
  */
-public class EPRegisterPlugin {
+public class EPRegisterPlugin implements PatmobPlugin {
+
+    @Override
+    public String getName() {
+        return "European Patent Register Plugin";
+    }
+
+    @Override
+    public void doJob() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
