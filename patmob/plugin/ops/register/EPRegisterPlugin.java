f9d17409e49d2bfa23d89be24ab0c6a2f84ddd9b
genemob <piotr@genemob.com>
2015-03-29 20:33:45 -0400

    some cleanup

--- patmob/plugin/ops/register/EPRegisterPlugin.java
+++ patmob/plugin/ops/register/EPRegisterPlugin.java
@@ -72,31 +72,6 @@ public class EPRegisterPlugin implements PatmobPlugin {
     }
     
     public void submitBiblioRequest(RegisterRequestParams searchParams) {
-//        ArrayList<String> patentList = new ArrayList<>();
-//        //read list of PNs from user-selected text file
-//        JFileChooser fc = new JFileChooser();
-//        int i = fc.showOpenDialog(null);
-//        if (i==JFileChooser.APPROVE_OPTION) {
-//            File patentListFile = fc.getSelectedFile();
-//            try {
-//                try (BufferedReader br = new BufferedReader(
-//                        new FileReader(patentListFile))) {
-//                    String line;
-//                    while ((line=br.readLine())!=null) {
-//                        if (line.contains(" ")) {
-//                            //skip kind code etc,
-//                            patentList.add(line.substring(0, line.indexOf(" ")));
-//                        } else {
-//                            patentList.add(line);
-//                        }
-//                    }
-//                }
-//            } catch (Exception x) {
-//                System.out.println("OpsRequestTestPlugin: " + x);
-//            }
-//        }
-//        String[] pubNums = patentList.toArray(new String[0]);
-//        RegisterRequestParams searchParams = new RegisterRequestParams(pubNums);
         RegisterRequest rr = new RegisterRequest(searchParams);
         searchParams = rr.submitCall();
         
@@ -104,6 +79,11 @@ public class EPRegisterPlugin implements PatmobPlugin {
         for (String row : rows) {
             System.out.println(row);
         }
+        
+        System.out.println("parseBiblioResults RETURNED " +
+                searchParams.getResultRows().size() + " results for " +
+                searchParams.getPatentNumbers().length + " requests.");
+        
     }
     
 }
